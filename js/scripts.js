321bda6008659a2b1d1d33a95460ed8ff212cc75
Joanna Strzelecka <joastr1@gmail.com>
2016-12-27 19:37:15 +0100

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+// plik scripts.js
+
+function Button(text) {
+	this.text = text || 'Hello';
+}
+
+Button.prototype = {
+	create: function() {
+	var self = this;
+	this.$element = $('<button>');
+	this.$element.text(this.text);
+	this.$element.click(function() {
+		alert(self.text);
+	});
+
+	$('body').append(this.$element);
+		
+	}
+}
+
+var btn1 = new Button('Hello!');
+btn1.create(); 
+
+
+function Button(text) {
+	this.text = text || 'Hello';
+}
+
+Button.prototype = {
+	create: function() {
+		this.$element = $('<button>');
+		this.$element.text(this.text);
+		this.$element.click(function() {
+		alert(this.text);} 
+		.bind(this));
+		$('body').append(this.$element);
+		
+	}
+}
+
+var btn1 = new Button('Hello!');
+btn1.create(); 
+
+
+
+
+
+
+
